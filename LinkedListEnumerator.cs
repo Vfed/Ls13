c1d0ec02b5a847842230833d636ab71dd2483bf7
Vfed <asser.282195@gmail.com>
2021-09-02 19:21:59 +0300

    Add project files.

---
+++ LinkedListEnumerator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ls13
+{
+    //class ListElementsEnumerator<T> : IEnumerator<T>
+    //{
+
+    //    public T Value { get; set; }
+    //    int position = -1;
+    //    public ListElementsEnumerator(T elem)
+    //    {
+    //        this.Value = elem;
+    //    }
+    //    public object Current
+    //    {
+    //        get
+    //        {
+    //            return Value;
+    //        }
+    //    }
+
+    //    public bool Next()
+    //    {
+    //        if (position < days.Length - 1)
+    //        {
+    //            position++;
+    //            return true;
+    //        }
+    //        else
+    //            return false;
+    //    }
+
+    //    public void Reset()
+    //    {
+    //        position = -1;
+    //    }
+    //}
+    //public class ListElement<T> : IEnumerator<T>
+    //{
+    //    public T Value { get; set; }
+    //    public ListElement<T> Prev { get; set; }
+    //    public ListElement<T> Next { get; set; }
+    //    public IEnumerator<T> GetEnumerator()
+    //    {
+    //        //return Value<T>.GetEnumerator();
+    //        return ListElement<T>.GetEnumerator();
+    //    }
+    //}
+
+    ////public interface IEnumerable<T>
+    ////{
+    ////    IEnumerator<T> GetEnumerator();
+    ////}
+
+    //public interface IEnumerator<T> 
+    //{ 
+    //    public T Value { get; set; }
+    //    public ListElement<T> Prev { get; set; }
+    //    public ListElement<T> Next { get; set; }
+    //}
+
+    //class LinkedListEnumerator<T> : IEnumerator<T>
+    //{
+    //    public T Value { get; set; }
+    //    public ListElement<T> Prev { get; set; }
+    //    public ListElement<T> Next { get; set; }
+    //    public IEnumerator<T> GetEnumerator()
+    //    {
+    //        //return Value<T>.GetEnumerator();
+    //        //return GetEnumerator();
+    //    }
+    //}
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Ls13
+{
+    class Program
+    {
+        int[,] mas = new int[5, 5];
+        static void Main(string[] args)
+        {
+            int[,] mas = new int[5, 5];
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {
+                    mas[i, j] = (i+1) *(j+1);
+                }
+            }
+            Console.WriteLine();
+            //1
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {
+                    Console.Write(mas[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            //2
+            int a = 0;
+            foreach (var item in mas)
+            {
+                a++;
+                if (a % mas.GetLength(1) != 0)
+                {
+                    Console.Write(item + " ");
+                }
+                else
+                {
+                    Console.WriteLine(item + " ");
+                }
+            }
+            Console.WriteLine();
+            int s = 0;
+            int k = 0;
+            //3
+            do {
+                Console.Write(mas[s,k] + " ");
+                k++;
+                if (k == mas.GetLength(1)) 
+                {
+                    k = 0;
+                    s++;
+                Console.WriteLine();
+                }
+            } while (s < mas.GetLength(0));
+            //4
+
+        }
+    }
+
+}
